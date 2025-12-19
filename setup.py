72b0c6477561afcebe40fdf61103f6daa49d7cb9
Jayadev Joshi <jaidev53ster@gmail.com>
2025-12-19 15:19:23 -0500

    minifest file added

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="r2g2",
     version="0.1.0",
@@ -18,6 +21,15 @@ setup(
     },
     author="Jayadev Joshi",
     description="A tool to convert R scripts and packages to Galaxy wrappers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BlankenbergLab/r2g2",
     license="MIT",
     license_files=["LICENSE"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
